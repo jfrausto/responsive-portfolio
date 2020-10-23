48f68846f3affa3a17a336a089d311039bbf3db9
jessef <1319957+jfrausto@users.noreply.github.com>
2020-10-23 10:27:21 -0700

    hover animate nav buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,17 @@ const navContact = document.querySelector("#contact");
 document.addEventListener("DOMContentLoaded", function(){
     console.log("loaded");
 
-
+    // event listeners for nav bar
     navAbout.addEventListener("click", function(){
         console.log("i was about");
+        window.location.assign("./index.html")
     });
     navPortfolio.addEventListener("click", function(){
         console.log("i was portfolio");
+        window.location.assign("./portfolio.html")
     });
     navContact.addEventListener("click", function(){
         console.log("i was contact");
+        window.location.assign("./contact.html")
     });
   });
